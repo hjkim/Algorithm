42caef50d22d4238f39ea099a29d9e51b859b052
hjkim <wlrhkvlf23@gmail.com>
2019-03-08 02:57:15 +0900

    Edit Java File to Reduce Overhead

--- src/SortingAlgorithm/QuickSort/BOJ_2750_SortingNumber_QuickSort.java
+++ src/SortingAlgorithm/QuickSort/BOJ_2750_SortingNumber_QuickSort.java
@@ -1,15 +1,20 @@
 package CodeTraining;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /*
  * [문제]
  * 1. N개의 수가 주어졌을 때, 이를 오름차순으로 정려하는 프로그램을 작성하시오.
+ * [주의]
+ * 1. 출력량이 많아 IO 오버해드를 줄여줄 필요가 있다.
  */
 public class BOJ_2750_SortingNumber_QuickSort {
 	public static BufferedReader br;
+	public static BufferedWriter bw;
 	public static int N;
 	public static void main(String args[]) throws IOException{
 		int arr[];
@@ -19,6 +24,7 @@ public class BOJ_2750_SortingNumber_QuickSort {
 	}
 	public static int[] initSetting()throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		N = Integer.parseInt(br.readLine());
 		int tmp[] = new int[N];
 		for(int i=0; i<N; i++) {
@@ -42,11 +48,11 @@ public class BOJ_2750_SortingNumber_QuickSort {
 		int pivot = A[(left + right)/2];
 		
 		while(left <= right) {
-			while(A[left] < pivot) {
+			while(left <= right && A[left] < pivot) {
 				left++;
 			}
 			
-			while(A[right] > pivot) {
+			while(left <= right && A[right] > pivot) {
 				right--;
 			}
 			if(left <= right) {
@@ -64,10 +70,13 @@ public class BOJ_2750_SortingNumber_QuickSort {
 		A[right] = tmp;
 	}
 	
-	public static void printArray(int A[]) {
+	public static void printArray(int A[]) throws IOException{
 		for(int i=0; i<N; i++) {
-			System.out.println(A[i]);
+			//System.out.println(A[i]);
+			bw.write(String.valueOf(A[i])+"\n");
 		}
+		bw.flush();
+		bw.close();
 	}
 	
 }
